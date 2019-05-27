b5dd2fd73b2cf652385d0e597b19db4395c1967e
weiqianlai <2314604007@qq.com>
2019-05-27 20:58:28 +0800

    2019-5-27
    接下来完善地图导航功能

--- ky-parking-miniapp/pages/map/map.js
+++ ky-parking-miniapp/pages/map/map.js
@@ -1,42 +1,62 @@
 Page({
   data: {
-    Height: 0,
-    scale: 13,
-    latitude: "",
-    longitude: "",
-    markers: [],
-    // controls: [{
-    //   id: 1,
-    //   iconPath: '/assests/imgs/jian.png',
-    //   position: {
-    //     left: 320,
-    //     top: 100 - 50,
-    //     width: 20,
-    //     height: 20
-    //   },
-    //   clickable: true
-    // },
-    // {
-    //   id: 2,
-    //   iconPath: '/assests/imgs/jia.png',
-    //   position: {
-    //     left: 340,
-    //     top: 100 - 50,
-    //     width: 20,
-    //     height: 20
-    //   },
-    //   clickable: true
-    // }
-    // ],
-    circles: []
+    scale: 17,
+    latitude: "26.527530",
+    longitude: "107.892078",
+    markers: [{
+        id: "1",
+        latitude: 26.527252,
+        longitude: 107.894073,
+        width: 30,
+        height: 30,
+        iconPath: "../../images/map.png",
+        callout: {
+          content: "实验楼地下停车去",
+          padding: 8,
+          display: 'ALWAYS',
+          textAlign: 'center'
+        }
+
+      },
+      {
+        id: 2,
+        latitude: 26.526743,
+        longitude: 107.894078,
+        width: 30,
+        height: 30,
+        iconPath: "../../images/map.png",
+        callout: {
+          content: "1号停车区",
+          padding: 8,
+          display: 'ALWAYS',
+          textAlign: 'center'
+        }
+
+      },
+      {
+        id: 2,
+        latitude: 26.528860,
+        longitude: 107.890661,
+        width: 30,
+        height: 30,
+        iconPath: "../../images/map.png",
+        callout: {
+          content: "体育馆停车区",
+          padding: 8,
+          display: 'ALWAYS',
+          textAlign: 'center'
+        }
+      }
+    ],
 
   },
 
-  onLoad: function () {
+  onLoad: function() {
     var _this = this;
 
+    console.log("----" + this.data.markers);
     wx.getSystemInfo({
-      success: function (res) {
+      success: function(res) {
         //设置map高度，根据当前设备宽高满屏显示
         _this.setData({
           view: {
@@ -50,78 +70,51 @@ Page({
       }
     })
 
-    wx.getLocation({
-      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
-      success: function (res) {
+  },
 
-        _this.setData({
-          latitude: res.latitude,
-          longitude: res.longitude,
-          markers: [{
-            id: "1",
-            latitude: res.latitude,
-            longitude: res.longitude,
-            width: 30,
-            height: 30,
-            iconPath: "../../images/map.png",
-            title: "1号停车场"
+  showDialogBtn: function(e) {
+    var user_id = e.target.dataset.id;
+    console.log("uId" + user_id)
 
-          }],
-          // circles: [{
-          //   latitude: res.latitude,
-          //   longitude: res.longitude,
-          //   color: '#FF0000DD',
-          //   fillColor: '#7cb5ec88',
-          //   radius: 3000,
-          //   strokeWidth: 1
-          // }]
+    this.setData({
 
-        })
-      }
+      showModal: true,
 
     })
 
+    console.log("SSS"+e.markers);
+
   },
 
-  regionchange(e) {
-    console.log("regionchange===" + e.type)
+  hideModal: function() { //隐藏模态对话框
+
+    this.setData({
+
+      showModal: false
+
+    });
+
   },
 
-  //点击merkers
-  markertap(e) {
-    console.log(e.markerId)
+  /**
+  
+  * 生命周期函数--监听页面加载
+  
+  */
 
-    wx.showActionSheet({
-      itemList: ["A"],
-      success: function (res) {
-        console.log(res.tapIndex)
-      },
-      fail: function (res) {
-        console.log(res.errMsg)
-      }
-    })
+
+
+
+  showmodle: function(e) {
+    this.setData({
+
+      showModal: false
+
+    });
+
   },
 
   //点击缩放按钮动态请求数据
-  controltap(e) {
-    var that = this;
-    console.log("scale===" + this.data.scale)
-    if (e.controlId === 1) {
-      // if (this.data.scale === 13) {
-      that.setData({
-        scale: --this.data.scale
-      })
-      // }
-    } else {
-      //  if (this.data.scale !== 13) {
-      that.setData({
-        scale: ++this.data.scale
-      })
-      // }
-    }
-
-
-  },
 
 
 })
